9525886a53f7353884f6601f1f28b13bd4b7638c
Stuart Walsh <stu@ipng.org.uk>
2016-03-09 18:57:23 +0000

    initial import

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Is HyperRPG Live Yet?' });
+});
+
+module.exports = router;
