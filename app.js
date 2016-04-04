03c19c07ce030d0a17d1d6da2cfce9200dfed841
Drew Kario <kario@apostrophed.com>
2016-04-04 12:55:18 -0400

    Fixed scaling issues on offline site, repositioned object with js, wrapped over and counters in div

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
+app.listen(3005);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
